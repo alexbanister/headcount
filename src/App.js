2483c05b18851e1fd31956bf9aa55b13b5b77adf
Luke Finney <lucaswfinney@gmail.com>
2017-10-03 16:05:16 -0600

    Build core React components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import CardContainer from './CardContainer';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      districts: new DistrictRepository(kinderData)
+    }
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <h1>Welcome To Headcount 2.0</h1>
+        {/* <Controls />
+        <Compare /> */}
+        <CardContainer districtData={this.state.districts} />
+      </div>
     );
   }
 }
